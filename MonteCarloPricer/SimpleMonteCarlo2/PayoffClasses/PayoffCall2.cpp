c84772e445cf6c2e3d4bf779e2210374268fcd08
hugobergeret <bergerethugo@yahoo.fr>
2021-12-09 15:11:38 +0100

    Delete MonteCarloPricer directory

--- MonteCarloPricer/SimpleMonteCarlo2/PayoffClasses/PayoffCall2.cpp
+++
@@ -1,9 +0,0 @@
-#include "PayoffCall2.h"
-
-PayOffCall::PayOffCall(double Strike_) : Strike(Strike_)
-{ }
-
-double PayOffCall::operator () (double Spot) const
-{
-    return (Spot - Strike) >0 ? (Spot - Strike) : 0;
-}
--- MonteCarloPricer/SimpleMonteCarlo2/PayoffClasses/PayoffPut.cpp
+++
@@ -1,9 +0,0 @@
-#include "PayoffPut.h"
-
-PayoffPut::PayoffPut(double Strike_) : Strike(Strike_)
-{ }
-
-double PayoffPut::operator () (double Spot) const
-{
-    return (Strike - Spot) >0 ? (Strike - Spot) : 0;
-}
